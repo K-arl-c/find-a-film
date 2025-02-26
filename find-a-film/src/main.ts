b71fc399a3b624efd9b5426ab717445aa90a4dbd
Karl <karlcampbell100@gmail.com>
2025-02-26 15:46:35 +0000

    exported apicalls to main to allow for cleaner main

--- find-a-film/src/main.ts
+++ find-a-film/src/main.ts
@@ -1,4 +1,5 @@
-import './style.css'
+import './style.css';
+import {getMovieDetails, addMovie} from "./apicall";
 
 
 // Function that does an api call to display all movies
@@ -22,90 +23,16 @@ import './style.css'
 
 // A function that displays a random movie when button is clicked
 
-
-let savedMovies:any = null;
-
-const fetchMovies = async () =>{
-
-
-    if(savedMovies != null){
-        return savedMovies;
-    }
-
-    try{
-    const response = await fetch ("http://localhost:8080/movies");
-    if(!response.ok){
-        throw new Error("Could not fetch API :(");
-    }
-    const data = await response.json();
-
-    savedMovies = data;
-    return savedMovies;
-    }
-    catch(error){
-        console.error("There has been an error", error);
-    }
-}
-
-
-const getMovieDetails = async (i:any) =>{
-    try{
-    const movies = await fetchMovies();
-    if(movies[i]){
-        const movie = movies[i];
-        console.log("Movie Title:", movie.title);
-        console.log("Movie Genre:", movie.genre);
-        console.log("Release Year:", movie.releaseYear);
-        console.log("Rating:", movie.rating);
-        console.log("Uploaded By:", movie.uploadedBy);
-        console.log("Image URL:", movie.imageURL);
-        console.log("Description:", movie.description);    
-        } else{
-            console.log("No Movie found")
-        }
-    } catch (error){
-        console.error("There has been an error",error)
-    }
-};
-
-// getMovieDetails(0);
-
-
-const addMovie = async (movieDetails: {
-    title: string, 
-    genre: string, 
-    releaseYear: string, 
-    rating: string,
-    uploadedBy: string,
-    imageURL: string,
-    description: string }) =>{
-        try{
-            const response = await fetch ("http://localhost:8080/movies", {
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(movieDetails),
-            });
-            if(!response.ok){
-                throw new Error("Failure to create movie");
-            }
-            console.log("Movie created successfully");
-        } 
-        catch (error){
-        console.error("There has been an error",error)
-}
-} 
-
 const testMovie = {
-  "title": "Elf",
-  "genre": "Comedy",
-  "releaseYear":"2003",
-  "rating": "7",
+  "title": "Inception",
+  "genre": "Sci-Fi",
+  "releaseYear":"2010",
+  "rating": "8",
   "uploadedBy": "Karl",
   "imageURL": "https://www.google.com",
-  "description": "Buddy, a human, is raised amongst elves at the North Pole. When he discovers that he is not an elf, he travels to New York to search for his biological father."
+  "description": "A skilled thief, who specializes in stealing secrets from deep within the subconscious, is offered a chance to have his past crimes forgiven if he can successfully plant an idea into someone's mind."
 };
 
 addMovie(testMovie);
 
+getMovieDetails(0);
