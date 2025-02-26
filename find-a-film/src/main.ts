329d4f1d49de72a4f822bbe3869e510b916ac1a3
Karl <karlcampbell100@gmail.com>
2025-02-26 12:57:42 +0000

    fixed fetchmovies function to include caching to avoid repeated api calls. Created a getmoviedetails function that will show all of the details of 1 specific movie if provided an index

--- find-a-film/src/main.ts
+++ find-a-film/src/main.ts
@@ -1,1 +1,65 @@
 import './style.css'
+
+// API Call to fetch all information relating to all movies - using a caching statement to 
+// avoid repeated apicalls
+
+let savedMovies:any = null;
+
+const fetchMovies = async () =>{
+
+
+    if(savedMovies != null){
+        return savedMovies;
+    }
+
+    try{
+    const response = await fetch ("http://localhost:8080/movies");
+    if(!response.ok){
+        throw new Error("Could not fetch API :(");
+    }
+    const data = await response.json();
+
+    savedMovies = data;
+    return savedMovies;
+    }
+    catch(error){
+        console.error("There has been an error", error);
+    }
+}
+
+fetchMovies();
+
+const getMovieDetails = async (i:any) =>{
+    const movies = await fetchMovies();
+    if(movies[i]){
+        return movies[i];
+    } else{
+        console.log("no movie found")
+        return null;
+    }
+}
+
+getMovieDetails(0).then((movie)=>console.log(movie));
+
+// Function that does an api call to display all movies
+
+// Function that does a post to an api to create a movie based off all 
+// information provided by the user 
+// Username 
+// Title, 
+// Genre, 
+// Release Year 
+// Rating
+
+
+// Function that does a post to an api to create a reivew based off all information provided 
+// by the user  
+// Review Description 
+// Rating 
+// Username
+
+// A Function that calls api getMovie based on provided search string
+
+// A function that displays a random movie when button is clicked
+
+
