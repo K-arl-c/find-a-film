a9f24d6ffc7e9d0ff852cd06c0fd24a2be49701f
Karl <karlcampbell100@gmail.com>
2025-02-27 20:55:16 +0000

    added a fetchcurrentmovie and a function to get this movies details by storing it in an object ready for DOM manipulation

--- find-a-film/src/reviews.ts
+++ find-a-film/src/reviews.ts
@@ -1,8 +1,38 @@
 const reviewerName = document.querySelector<HTMLInputElement>("#userName");
 const reviewDescription = document.querySelector<HTMLInputElement>("#review");
 const reviewRating = document.querySelector<HTMLSelectElement>("#rating");
+const submitButton = document.querySelector<HTMLButtonElement>("#submit-button");
 const urlParam = new URLSearchParams(window.location.search);
 const movieId = urlParam.get("id");
+
+
+
+// get selected movie
+const fetchCurrentMovie = async () =>{
+    try{
+    const response = await fetch (`http://localhost:8080/movies/${movieId}`);
+    if(!response.ok){
+        throw new Error("Could not fetch API :(");
+    }
+    const data = await response.json();
+    return data;
+    }
+    catch(error){
+        console.error("There has been an error", error);
+    }
+}
+
+const getCurrentMovieDetails = async () =>{
+    try{
+        const currentMovie = await fetchCurrentMovie();
+        console.log(currentMovie);
+    } catch (error){
+        console.error("Error retreiving current film ",  error)
+    }
+}
+
+getCurrentMovieDetails();
+
 // Add review
 
 console.log("Movie ID is " + movieId);
@@ -13,7 +43,7 @@ export const addReview = async (userDetails: {
     review: string, 
     }) =>{
         try{
-            const response = await fetch ("http://localhost:8080/reviews/2", {
+            const response = await fetch (`http://localhost:8080/reviews/${movieId}`, {
                 method: "POST",
                 headers:{
                     "Content-Type": "application/json",
@@ -29,3 +59,26 @@ export const addReview = async (userDetails: {
         console.error("There has been an error",error)
 }
 } 
+
+
+
+
+
+
+
+
+
+
+submitButton?.addEventListener("click", () =>{
+    const userReview = {
+  "review": reviewDescription?.value || "",
+  "rating": reviewRating?.value || "",
+  "addedBy": reviewerName?.value || "",
+};
+    addReview(userReview);
+    reviewDescription!.value = ""
+    reviewRating!.value = ""
+    reviewerName!.value = ""
+
+});
+
