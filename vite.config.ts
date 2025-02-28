72ff8477beb091e03b52c1ded978257d023734f2
Karl <karlcampbell100@gmail.com>
2025-02-28 12:21:21 +0000

    added all pages into vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
-import {defineConfig} from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: "/find-a-film",
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',  
+        addMovie: './addMovie.html',
+        review: './src/review.html',
+      }
+    }
+  }
 });
